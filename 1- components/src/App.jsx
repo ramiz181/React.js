a23a18323054eb80598d2fe572e002ed7dc648b2
ramiz181 <ramizmalik78685@gmail.com>
2026-02-15 01:14:24 +0500

    conditional rendering

--- 1- components/src/App.jsx
+++ 1- components/src/App.jsx
@@ -1,17 +1,30 @@
 export default function App() {
 
+  // let listItems = []
   let listItems = ['react tutorial', 'construction website', 'client hunting series', 'linkedin post']
 
   return (
     <>
       <h1>Ramiz Malik - Fullstack dev</h1>
+      {listItems.length === 0 ? <h3>Need list to render</h3> :
+        <ul>
+          {listItems.map((item, index) => {
+            return <li key={index}>
+              {item}
+            </li>
+          })}
+        </ul>}
+
+      {/* OR */}
+
+      {/* {listItems.length === 0  <h3>Need list to render</h3>}
       <ul>
         {listItems.map((item, index) => {
           return <li key={index}>
             {item}
           </li>
         })}
-      </ul>
+      </ul> */}
     </>
   )
 }
